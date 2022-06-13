d8d42dc20e82e25dfe4fcd47b8573986466ae9d2
gustavoanjoz <gugass.gu@gmail.com>
2022-06-13 08:34:19 -0300

    second commit

--- app/src/main/java/com/example/appnavigation/SecondFragment.kt
+++ app/src/main/java/com/example/appnavigation/SecondFragment.kt
@@ -7,16 +7,16 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 
-class FirstFragment : Fragment() {
+class SecondFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_first, container, false)
+        val view = inflater.inflate(R.layout.fragment_second, container, false)
 
-        view.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_firstFragment_to_secondFragment) }
+        view.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_secondFragment_to_firstFragment) }
 
         return view
     }
